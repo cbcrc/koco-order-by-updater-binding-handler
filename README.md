67d8d6ecb0e289bb36930aaeb3bc34242d2ad8a2
Mark Moyes <mark.moyes@cbc.ca>
2015-05-04 14:12:33 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # koco-order-by-updater-binding-handler
-Knockout binding handler for ordering lists
+Knockout binding handler for ordering lists.
+
+## Installation
+
+```bash
+bower install koco-order-by-updater-binding-handler
+```
+
+## Usage with KOCO
+
+This is a shared module that is used in many other modules. The convention is to require the handler in the `knockout-binding-handlers.js` file like so:
+
+```javascript
+define([
+  ...
+  'bower_components/koco-order-by-updater-binding-handler/src/koco-order-by-updater-binding-handler'
+  ...
+],
+```
+
+It also includes the `string-utilities` library as a bower dependency. You will need to configure an alias in the `require.configs.js` file for the dependency if you haven't already:
+
+```javascript
+paths: {
+  ...
+  'string-utilities': 'bower_components/koco-string-utilities/src/string-utilities',
+  ...
+}
+```
+
+`koco-order-by-updater-binding-handler` requires you to provide your own orderByFunction. To use it in a knockout template:
+```html
+<th>
+<a href="#" data-bind="orderByUpdater: pagingInfo, orderByUpdaterSettings: {argumentName:'AttributeName', orderByFunction: updateOrderBy, enabled:!sortable}">
+Column Name
+</a>
+</th>
+],
+```
---
+++ src/koco-order-by-updater-binding-handler.js
@@ -0,0 +1,55 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['knockout', 'jquery', 'string-utilities'],
+    function(ko, $, stringUtilities) {
+        'use strict';
+
+        var defaultSettings = {
+            argumentName: 'title',
+            orderByFunction: null,
+            orderByArgumentName: 'orderBy',
+            orderByDirectionArgumentName: 'orderByDirection',
+            enabled: true
+        };
+
+        //TODO: Permettre de spécifier orderByDirection par défaut (premier sens lorsque nouvellement sélectionner)
+        ko.bindingHandlers.orderByUpdater = {
+            init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+                var settings = $.extend({}, defaultSettings, allBindingsAccessor().orderByUpdaterSettings);
+                var $element = $(element);
+
+                if (settings.enabled) {
+                    $element.click(function(event) {
+                        event.preventDefault();
+                        settings.orderByFunction.call(viewModel, settings.argumentName, event);
+
+                        return false;
+                    });
+                } else {
+                    $element.removeAttr('href').changeElementType('span');
+                }
+            },
+            update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+                var settings = $.extend(defaultSettings, allBindingsAccessor().orderByUpdaterSettings),
+                    $element = $(element),
+                    pagingInfo = ko.unwrap(valueAccessor()),
+                    currentOrderByDirection = pagingInfo.orderByDirection,
+                    currentOrderBy = pagingInfo.orderBy;
+
+                if (settings.enabled) {
+                    $element.find('i').remove();
+                    $element.html($.trim($element.text()));
+
+                    if (stringUtilities.equalsIgnoreCase(currentOrderBy, settings.argumentName)) {
+                        //mettre la fleche
+                        if (stringUtilities.equalsIgnoreCase(currentOrderByDirection, 'descending')) {
+                            $element.append('&nbsp;<i class="fa fa-caret-down"></i>');
+                        } else {
+                            $element.append('&nbsp;<i class="fa fa-caret-up"></i>');
+                        }
+                    }
+                }
+            }
+        };
+    });
