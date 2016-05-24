f7f35596515c1cde7a15e2131085c176e20107b8
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 14:46:04 -0400

    update to koco2

---
+++ dist/koco-order-by-updater-binding-handler.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var _knockout = require('knockout');
+
+var _knockout2 = _interopRequireDefault(_knockout);
+
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+var _stringUtilities = require('string-utilities');
+
+var _stringUtilities2 = _interopRequireDefault(_stringUtilities);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var defaultSettings = {
+    argumentName: 'title',
+    orderByFunction: null,
+    orderByArgumentName: 'orderBy',
+    orderByDirectionArgumentName: 'orderByDirection',
+    enabled: true
+};
+
+//TODO: Permettre de spécifier orderByDirection par défaut (premier sens lorsque nouvellement sélectionner)
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+_knockout2.default.bindingHandlers.orderByUpdater = {
+    init: function init(element, valueAccessor, allBindingsAccessor, viewModel) {
+        var settings = _jquery2.default.extend({}, defaultSettings, allBindingsAccessor().orderByUpdaterSettings);
+        var $element = (0, _jquery2.default)(element);
+
+        if (settings.enabled) {
+            $element.click(function (event) {
+                event.preventDefault();
+                settings.orderByFunction.call(viewModel, settings.argumentName, event);
+
+                return false;
+            });
+        } else {
+            $element.removeAttr('href').changeElementType('span');
+        }
+    },
+    update: function update(element, valueAccessor, allBindingsAccessor, viewModel) {
+        var settings = _jquery2.default.extend(defaultSettings, allBindingsAccessor().orderByUpdaterSettings),
+            $element = (0, _jquery2.default)(element),
+            pagingInfo = _knockout2.default.unwrap(valueAccessor()),
+            currentOrderByDirection = pagingInfo.orderByDirection,
+            currentOrderBy = pagingInfo.orderBy;
+
+        if (settings.enabled) {
+            $element.find('i').remove();
+
+            if (_stringUtilities2.default.equalsIgnoreCase(currentOrderBy, settings.argumentName)) {
+                //mettre la fleche
+                if (_stringUtilities2.default.equalsIgnoreCase(currentOrderByDirection, 'descending')) {
+                    $element.append('<i class="fa fa-caret-down"></i>');
+                } else {
+                    $element.append('<i class="fa fa-caret-up"></i>');
+                }
+            }
+        }
+    }
+};
